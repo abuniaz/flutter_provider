2c08d231e6a265817ac4d931de18b7f13b9bd29e
Abu Niaz <niaz35-507@diu.edu.bd>
2023-01-20 23:44:21 +0600

    okay

--- lib/provider/count_provider.dart
+++ lib/provider/count_provider.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/foundation.dart';
 
-class CountProvider with ChangeNotifier {}
+class CountProvider with ChangeNotifier {
+  int _count = 0;
+
+  get count => _count;
+}
--- lib/screen/count_example.dart
+++ lib/screen/count_example.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_in_flutter/provider/count_provider.dart';
 
 class CountExample extends StatefulWidget {
   const CountExample({super.key});
@@ -10,13 +12,17 @@ class CountExample extends StatefulWidget {
 class _CountExampleState extends State<CountExample> {
   @override
   Widget build(BuildContext context) {
+    final countProvider = Provider.of<CountProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Count Example"),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("0"),
+      body: Center(
+        child: Text(
+          countProvider.count.toString(),
+          style: const TextStyle(fontSize: 50),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
