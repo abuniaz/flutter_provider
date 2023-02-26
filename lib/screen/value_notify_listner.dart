855426bfba6e042b671d446a3eee371a01a307f6
Abu Niaz <niaz35-507@diu.edu.bd>
2023-02-26 16:35:52 +0600

    Update value_notify_listner.dart

--- lib/screen/value_notify_listner.dart
+++ lib/screen/value_notify_listner.dart
@@ -4,6 +4,7 @@ class NotifyListnerScreen extends StatelessWidget {
   NotifyListnerScreen({super.key});
 
   ValueNotifier<int> _center = ValueNotifier<int>(0);
+  ValueNotifier<bool> passwd = ValueNotifier<bool>(true);
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +13,36 @@ class NotifyListnerScreen extends StatelessWidget {
         title: const Text('Stateless to Statefull'),
         centerTitle: true,
       ),
-      body: Center(
-          child: ValueListenableBuilder(
-              valueListenable: _center,
+      body: Column(
+        children: [
+          ValueListenableBuilder(
+              valueListenable: passwd,
               builder: (context, value, child) {
-                return Text(
-                  _center.value.toString(),
-                  style: TextStyle(fontSize: 18),
+                return TextFormField(
+                  obscureText: passwd.value,
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      suffix: InkWell(
+                        onTap: () {
+                          passwd.value = !passwd.value;
+                        },
+                        child: Icon(passwd.value
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility),
+                      )),
                 );
-              })),
+              }),
+          Center(
+              child: ValueListenableBuilder(
+                  valueListenable: _center,
+                  builder: (context, value, child) {
+                    return Text(
+                      _center.value.toString(),
+                      style: TextStyle(fontSize: 18),
+                    );
+                  })),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _center.value++;
