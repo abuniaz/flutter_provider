eb428ed800a3531ada582b992e59cab6436e7dbc
Abu Niaz <niaz35-507@diu.edu.bd>
2023-03-13 11:53:16 +0600

    Niaz

---
+++ lib/screen/login.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordControler = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Email'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
